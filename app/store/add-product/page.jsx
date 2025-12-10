1311eb02e3c3a91616ade7c254b8c4b21a352e74
Sumant246u <sahoosumant77@gmail.com>
2025-12-10 12:41:33 +0530

    add a two new component

---
+++ app/store/add-product/page.jsx
@@ -0,0 +1,79 @@
+'use client'
+
+import { assets } from "@/assets/assets"
+import Image from "next/image"
+import { useState } from "react"
+import toast from "react-hot-toast"
+
+
+export default function StoreAddProduct() {
+
+    const categories = ['Electronics', 'Clothing', 'Home & Kitchen', 'Beauty & Health', 'Toys & Games', 'Sports & Outdoors', 'Books & Media', 'Food & Drink', 'Hobbies & Crafts', 'Others']
+    const [images, setImages] = useState({ 1: null, 2: null, 3: null, 4: null })
+    const [productInfo, setProductInfo] = useState({
+        name: "",
+        description: "",
+        mrp: 0,
+        price: 0,
+        category: "",
+    })
+
+    const [loading, setLoading] = useState(false)
+
+    const onChangeHandler = (e) => {
+        setProductInfo({ ...productInfo, [e.target.name]: e.target.value })
+    }
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefult()
+
+        // Logic to add a product
+    }
+
+    return (
+        <form onSubmit={e=>toast.promise(onSubmitHandler(e),{loading:'Adding Product...'})} className="text-slate-500 mb-28">
+            <h1 className="text-2xl">Add New <span className="text-slate-800 font-medium">Products</span></h1>
+            <p className="mt-7">product Images</p>
+            <div htmlFor="" className="flex gap-3 mt-4">
+                {Object.keys(images).map((key) => (
+                    <label key={key} htmlFor={`images${key}`}>
+                        <Image width={300} height={300} className="h-15 w-auto border border-slate-200 rounded cursor-pointer" src={images[key] ? URL.createObjectURL(images[key]) : assets.upload_area} alt="" />
+                        <input type="file" accept="image/" id={`images${key}`} onChange={e => setImages({ ...images, [key]: e.target.files[0] })} hidden />
+                    </label>
+                ))}
+            </div>
+
+            <label htmlFor="" className="flex flex-col gap-2 my-6">
+                Name
+                <input type="text" placeholder="Enter Product name" name="name" onChange={onChangeHandler} value={productInfo.name} className="border border-slate-200 rounded w-full max-w-sm p-2 px-4 outline-none" required />
+            </label>
+
+            <label htmlFor="" className="flex flex-col gap-2 my-6">
+                Description
+                <textarea placeholder="Enter Product description" name="description" rows={5} onChange={onChangeHandler} value={productInfo.description} className="border border-slate-200 rounded w-full max-w-sm p-2 px-4 outline-none resize-none" required />
+            </label>
+
+            <div className="flex gap-5">
+                <label htmlFor="" className="flex flex-col gap-2">
+                    Actual Price ($)
+                    <input type="number" placeholder="0" name="mrp" rows={5} onChange={onChangeHandler} value={productInfo.mrp} className="border border-slate-200 rounded w-full max-w-sm p-2 px-4 outline-none" required />
+                </label>
+                <label htmlFor="" className="flex flex-col gap-2">
+                    Offer Price ($)
+                    <input type="number" placeholder="0" name="price" rows={5} onChange={onChangeHandler} value={productInfo.price} className="border border-slate-200 rounded w-full max-w-45 p-2 px-4 outline-none" required />
+                </label>
+            </div>
+
+             <select onChange={e=>setProductInfo({...productInfo,category:e.target.value})} value={productInfo.category} className="border border-slate-200 rounded w-full max-w-sm p-2 px-4 my-6 outline-none">
+                    <option value="">Select a category</option>
+                    {categories.map((category)=>(
+                        <option key={category} value={category}>{category}</option>
+                    ))}
+                </select>
+
+                <br />
+
+                <button disabled={loading} className="bg-slate-800 text-white px-6 mt-7 py-2 hover:bg-slate-900 rounded transition">Add Product</button>
+        </form>
+    )
+}
--- components/store/StoreLayout.jsx
+++ components/store/StoreLayout.jsx
@@ -26,11 +26,11 @@ const StoreLayout = ({ children }) => {
   return loading ? (
     <Loading />
   ) : isSeller ? (
-    <div className=''>
+    <div className='flex flex-col h-screen'>
       <Storenavbar />
-      <div className=''>
+      <div className='flex flex-1 items-start h-full overflow-y-scroll no-scrollbar'>
         <StoreSidebar storeInfo={storeInfo} />
-        <div className=''>
+        <div className='flex-1 h-full p-5 lg:pl-12 lg:pt-12 overflow-y-scroll'>
           {children}
         </div>
       </div>
