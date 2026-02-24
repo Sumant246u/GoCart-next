808a34e365482aa4d91c994388ba5d0cee284727
Sumant246u <sahoosumant77@gmail.com>
2026-02-24 18:45:25 +0530

    Remove Banner and Navbar components from PublicLayout

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -1,14 +1,10 @@
 'use client'
 
-import Banner from "@/components/Banner"
 import Footer from "@/components/Footer"
-import Navbar from "@/components/Navbar"
 
 export default function PublicLayout({ children }) {
     return (
         <>
-            <Banner />
-            <Navbar />
             {children}
             <Footer />
         </>
